eb165bddec4564c0479deeeecfa72b0f8a2ee23f
Nico Feletic <wi22b065@technikum-wien.at>
2023-06-28 17:17:50 +0200

    Initial commit

---
+++ src/main/java/com/example/feletic_exam/FeleticExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.feletic_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeleticExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeleticExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/feletic_exam/FeleticExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feletic_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeleticExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
